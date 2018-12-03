04dda739a6664d25c33c190599758e28e14e0273
Doug Bell <doug@preaction.me>
2018-12-02 20:05:52 -0600

    add tests and schema configuration

--- lib/Mojolicious/Plugin/DBIC.pm
+++ lib/Mojolicious/Plugin/DBIC.pm
@@ -11,13 +11,38 @@ our $VERSION = '0.001';
 =cut
 
 use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::Loader qw( load_class );
+use Scalar::Util qw( blessed );
 
 sub register {
     my ( $self, $app, $conf ) = @_;
     # XXX Allow multiple schemas
-    # XXX Allow configuring schema connect() method
-    $app->helper( schema => sub { $conf->{schema} } );
+    my $schema_conf = $conf->{schema};
+    if ( !$schema_conf && $app->can( 'config' ) ) {
+        $schema_conf = $app->config->{dbic}{schema};
+    }
+    $app->helper( schema => sub {
+        state $schema = _load_schema( $schema_conf );
+        return $schema;
+    } );
     push @{ $app->routes->namespaces }, 'Mojolicious::Plugin::DBIC::Controller';
+}
+
+sub _load_schema {
+    my ( $conf ) = @_;
+    if ( blessed $conf && $conf->isa( 'DBIx::Class::Schema' ) ) {
+        return $conf;
+    }
+    elsif ( ref $conf eq 'HASH' ) {
+        my ( $class, $args ) = %{ $conf };
+        if ( my $e = load_class( $class ) ) {
+            die sprintf 'Unable to load schema class %s: %s',
+                $class, $e;
+        }
+        return $class->connect( ref $args eq 'ARRAY' ? @$args : $args );
+    }
+    die sprintf "Unknown DBIC schema config. Must be schema object or HASH, not %s",
+        ref $conf;
 }
 
 1;
