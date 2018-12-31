3a6a9b19c86f5c308bd43ee5602bda1d09c1a732
Doug Bell <doug@preaction.me>
2018-12-31 17:00:06 -0600

    make the forms look a little nicer
    This is ripe for a library to be added in as a default layout to make it
    look really nice.

--- eg/myapp.pl
+++ eg/myapp.pl
@@ -76,16 +76,33 @@ __DATA__
 </p>
 
 @@ notes/edit.html.ep
+%= stylesheet begin
+    label, input[type=text], textarea {
+        display: block;
+        width: 50vw;
+    }
+    textarea {
+        height: 30vh;
+    }
+% end
 %= form_for current_route, method => 'POST', begin
     %= csrf_field
+    %= label_for 'title' => 'Title'
     %= text_field 'title'
+    %= label_for 'description' => 'Body'
     %= text_area 'description'
     %= submit_button
 % end
 
 @@ notes/delete.html.ep
-%= button_to 'Cancel' => 'notes.get'
-%= button_to 'Delete' => 'notes.delete', method => 'POST'
+%= stylesheet begin
+    form {
+        display: inline-block;
+    }
+% end
+<h1>Delete <%= $row->title %>?</h1>
+%= link_to 'Cancel' => 'notes.get'
+%= csrf_button_to 'Delete' => 'notes.delete', method => 'POST'
 
 @@ events/list.html.ep
 <ul>
